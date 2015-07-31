3d44617c67fd5e775bb69e574debf41ae4f3d909
qJake <jake@burgy.me>
2015-07-31 17:36:32 -0500

    Bugfixes.

--- pet-feeder-reminder.groovy
+++ pet-feeder-reminder.groovy
@@ -24,21 +24,22 @@ definition(
     author: "Jake Burgy",
     description: "Reminds you to feed your pets between two times in a certain day if a motion sensor has not moved within that timeframe.",
     category: "Pets",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Pets/App-FeedMyPet.png",
+    iconX2Url: "hhttps://s3.amazonaws.com/smartapp-icons/Pets/App-FeedMyPet@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Pets/App-FeedMyPet@2x.png")
 
 preferences {
 	section("1. Choose your pet feeder sensor.") {
-		input "feeder", "capability.contactSensor", title: "Sensor:", required: true
+		input "feeder1", "capability.contactSensor", title: "Which sensor?"
 	}
 	section("2. Specify when you feed your pets.") {
-		input "timefrom", "time", title: "Between:", required: true
-		input "timeto", "time", title: "And:", required: true
+		input "timefrom", "time", title: "Between..."
+		input "timeto", "time", title: "And..."
 	}
 	section("3. If I forget by the ending time, text me.") {
-		input "phone1", "phone", title: "Phone number:", required: true
-		input "phone2", "phone", title: "Secondary number: (Optional)", required: false
+    	input "msg", "text", title: "Message? (Optional)", description: "Oops! Don't forget to feed the pets!", required: false
+		input "phone1", "phone", title: "Phone number?"
+		input "phone2", "phone", title: "Secondary number? (Optional)", required: false
 	}
 }
 
@@ -49,26 +50,31 @@ def installed()
 
 def updated()
 {
-	unsubscribe() //TODO no longer subscribe like we used to - clean this up after all apps updated
 	unschedule()
 	schedule(timeto, "scheduleCheck")
 }
 
 def scheduleCheck()
 {
-	log.trace "scheduledCheck"
-
-	def from = new Date(timefrom)
 	def now = new Date()
-	def feederEvents = feeder.eventsBetween(from, now)
-	log.trace "Found ${feederEvents?.size() ?: 0} feeder events since $timefrom"
+	def from = Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSSZ", timefrom)
+    
+    // Reset date to current date
+    from.date = now.date
+    from.month = now.month
+    from.year = now.year
+    
+	def feederEvents = feeder1.eventsBetween(from, now)
 	def feederOpened = feederEvents.count { it.value && it.value == "open" } > 0
 
-	if (feederOpened) {
-		log.debug "Feeder was opened since $timefrom, no SMS required"
-	} else {
-		log.debug "Feeder was not opened since $timefrom, texting $phone1 (and optionally, $phone2)"
-		sendSms(phone1, "Oops! Don't forget to feed the pets!")
-		sendSms(phone2, "Oops! Don't forget to feed the pets!")
+	def textMsg = msg ? msg : "Oops! Don't forget to feed the pets!"
+
+	if (!feederOpened)
+    {
+		sendSms(phone1, textMsg)
+        if(phone2)
+        {
+			sendSms(phone2, textMsg)
+        }
 	}
 }
